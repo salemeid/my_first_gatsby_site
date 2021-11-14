fd3518227e35e0805999d5d36e6a24116927de79
Salem Eid <salem.eid@gmail.com>
2021-11-14 23:53:31 +0300

    finished part 2 tutorial

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+
+const AboutPage = () => {
+  return (
+      <Layout pageTitle="About me">
+        <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      </Layout>   
+    
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
---
+++ src/pages/hobbies.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+
+const MyHobby = () => {
+  return (
+      <Layout pageTitle="My hobbies">
+        <p>I love sports.</p>
+      </Layout>   
+    
+  )
+}
+
+// Step 3: Export your component
+export default MyHobby
