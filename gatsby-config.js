5499711b2de609f5b94afcbc5876a97092cfa0cb
Salem Eid <salem.eid@gmail.com>
2021-11-16 22:36:45 +0300

    added MDX contents

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,18 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "My first gatsby site",
+    title: "Welcome Home!",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+      `gatsby-plugin-mdx`,
   ],
 };
