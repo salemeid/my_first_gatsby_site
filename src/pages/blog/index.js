f1f4de75c710f501f7f0ecfd8fbc4e7173294120
Salem Eid <salem.eid@gmail.com>
2021-11-17 00:29:30 +0300

    added links to posts using mdx API

---
+++ src/pages/blog/index.js
@@ -0,0 +1,42 @@
+import * as React from 'react'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
+
+const BlogPage = ({ data }) => {
+    console.log(data)   
+  return (
+    <Layout pageTitle="My Blog Posts">
+      {
+        data.allMdx.nodes.map(node => (
+                           
+          <article key={node.id}>
+            <h2>
+              <Link to={`/blog/${node.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
+            <p>Posted: {node.frontmatter.date}</p>
+          </article>
+          
+        ))
+      }
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMdx {
+        nodes {
+          frontmatter {
+            date
+            title
+          }
+          slug
+          id
+        }
+      }
+  }
+`
+
+export default BlogPage
